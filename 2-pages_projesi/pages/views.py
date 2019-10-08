4bb8b2ee020b707364f9e62d6d9f5f61ff263ffc
dataokulu <55794407+dataokulu@users.noreply.github.com>
2019-10-08 12:07:40 +0300

    Add files via upload

---
+++ 2-pages_projesi/pages/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView
+
+# Create your views here.
+
+class AnasayfaView(TemplateView):
+    template_name='home.html'
+
+class HakkindaView(TemplateView):
+    template_name='hakkinda.html'
